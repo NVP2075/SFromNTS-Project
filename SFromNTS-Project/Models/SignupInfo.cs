6f1db275895f351419d892854fd7115a24741e23
NVP2075 <vanphapnguyen79@gmail.com>
2026-09-25 23:13:54 +0700

    Upate signup code

--- SFromNTS-Project/Models/SignupInfo.cs
+++ SFromNTS-Project/Models/SignupInfo.cs
@@ -7,5 +7,9 @@
         public string Password { get; set; } = null!;
 
         public string Email { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string UserSurname {  get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public string Occupation { get; set; }
     }
 }
