8720fab180e83f63bf742b0fcdc5685caafb01de
NVP2075 <vanphapnguyen79@gmail.com>
2026-09-25 23:13:09 +0700

    Update signup code

--- SFromNTS-Project/Controllers/UserAccountController.cs
+++ SFromNTS-Project/Controllers/UserAccountController.cs
@@ -18,16 +18,15 @@ namespace SFromNTS_Project.Controllers
         public IActionResult SignUp([FromBody]SignupInfo sUI )
         {
             if (!ModelState.IsValid)
-                return View(sUI);
+                return Json(new { success = false, message = "Vui lòng điền đầy đủ thông tin!" });
             var isExist = _context.UserAccounts.Any(u => u.Email == sUI.Email);
             if (isExist)
             {
-                ModelState.AddModelError("Email", "Email này đã có tài khoản sử dụng!");
-                return View(sUI);
+                return Json(new { success = false, message = "Email này đã có tài khoản, vui lòng đăng nhập lại!" });
             }
 
 
-            UserAccount newUC = new UserAccount();
+            UserAccount newUC = new UserAccount(); 
             newUC.AccountName = sUI.AccountName;
             newUC.HashedPassword = BCrypt.Net.BCrypt.HashPassword(sUI.Password);
             newUC.CreateDate = DateTime.Now;
@@ -37,7 +36,7 @@ namespace SFromNTS_Project.Controllers
 
             _context.Add(newUC);
             _context.SaveChanges();
-            return View();
+            return Json(new { success = true });
         }
       
     }
--- SFromNTS-Project/obj/Debug/net9.0/SFromNTS-Project.AssemblyInfo.cs
+++ SFromNTS-Project/obj/Debug/net9.0/SFromNTS-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SFromNTS-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec410f16898b215a21259490dbbaf7fae8788e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31fa7925a972eb65dd96a0d3ddbb38b7dcdc7efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SFromNTS-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SFromNTS-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
